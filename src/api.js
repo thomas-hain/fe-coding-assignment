cef38d5f676ac3525d1e1119c4a0dd585bb175c4
Thomas Hain <131459580+thomas-hain@users.noreply.github.com>
2024-10-21 15:00:39 +0200

    chore: fixed & updated api url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://ebkqjitsgh.execute-api.eu-central-1.amazonaws.com/prod';
+const BASE_URL = 'https://2yahugzd4a.execute-api.eu-central-1.amazonaws.com/test';
 
 export function getListings() {
   return axios(`${BASE_URL}/listings/`);
