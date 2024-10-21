86bc3e2860b88ec09745e4c5afdef20f8af50d7e
Thomas Hain <131459580+thomas-hain@users.noreply.github.com>
2024-10-21 15:01:53 +0200

    chore: resolved problem with empty listing array being returned

--- src/api.js
+++ src/api.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 const BASE_URL = 'https://2yahugzd4a.execute-api.eu-central-1.amazonaws.com/test';
 
 export function getListings() {
-  return axios(`${BASE_URL}/listings/`);
+  return axios(`${BASE_URL}/listings`);
 }
